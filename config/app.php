89aa8190df3376c7c2b5b51fabf1e3b308763ba9
ccseptian <ccseptian@gmail.com>
2016-06-09 19:42:12 +0700

    2%

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        GrahamCampbell\Exceptions\ExceptionsServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
